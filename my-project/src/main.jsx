0ca13cc4fdd0665e1000fedde61f368710ce7466
Stariel Isaac <starielisaac@gmail.com>
2023-08-11 16:35:20 -0300

    feat: implemented react-router

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sitemap from './Sitemap'; // Importe o componente Sitemap
+import App from './App'; // Importe o componente App
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes> {/* Use o componente Routes aqui */}
+        <Route path="/sitemap.xml" element={<Sitemap />} />
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode> 
+);
